653e276f409dbf3a91ab30b907eb30f6336a8c48
Clinton's MiniPC <achuworifung@gmail.com>
2023-10-18 14:26:26 -0700

    added query class

--- src/ToDB/Query.java
+++ src/ToDB/Query.java
@@ -1,6 +1,87 @@
 package ToDB;
 
+import com.mongodb.*;
+import static com.mongodb.client.model.Filters.eq;
+
+import java.nio.file.DirectoryStream.Filter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalTime;
+import java.util.Date;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import static com.mongodb.client.model.Filters.eq;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;
+
 public class Query {
 	String uri = "mongodb+srv://achuworifung:QqgHwlf9hnQl53fW@cluster0.fodlvul.mongodb.net/";
+	MongoClient mongoClient;
+	MongoDatabase database;
+	MongoCollection<Document> collection;
+
+	public void close() {
+		if(mongoClient == null)  return;
+		mongoClient.close();
+		mongoClient = null;
+		database = null;
+		collection = null;
+	}
+
+	public void reopen(String collectionName) {
+		try {
+			mongoClient = MongoClients.create(uri);
+			database = mongoClient.getDatabase("EffortLoggerv2");
+			collection = database.getCollection(collectionName);
+		} catch (MongoException e) {
+			System.out.println("connection failed");
+		}
+
+	}
+	public void StartEfforts(String projectType, Date date, LocalTime startTime, String lifeCycleStep, String effortCategory )
+	
+	{
+		//close current mongodb client
+		
+		close();
+		reopen("Efforts");
+		try {
+			InsertOneResult result = collection.insertOne(new Document()
+					.append("id", new ObjectId())
+					.append("Project", projectType)
+					.append("Date", date.toString())
+					.append("Start Time", startTime.toString())
+					.append("End Time", "")
+					.append("Life Cyle Step", lifeCycleStep)
+					.append("effort Category", effortCategory));
+		}catch(MongoException e)
+		{
+			System.out.println("Failed to Insert Effort");
+		}
+		
+	}
+	public void endEffort(LocalTime endTime )
+	
+	{
+		//close current mongodb client
+		
+		close();
+		reopen("Efforts");
+		try {
+			Document document = collection.find(eq("End Time", "")).first();
+			collection.updateOne(document, new Document("$set", new Document("End Time", endTime.toString())));
+		}catch(MongoException e)
+		{
+			System.out.println("no Active document found");
+		}
+		
+	}
+
 
 }
+
