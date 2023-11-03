7eca63aae76a0d344ac92ed4d788e8201d78e0cd
Clinton's MiniPC <achuworifung@gmail.com>
2023-11-02 22:34:37 -0700

    fixed warning in definition.user

--- src/definitions/User.java
+++ src/definitions/User.java
@@ -6,9 +6,9 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class User {
 	
-	private SimpleStringProperty projectName;
+	public SimpleStringProperty projectName;
 	public IntegerProperty id;
-	private Integer colThree, col1, col2, col4, col5, col6, col7, col8, col9, col10, col11, col12,
+	public Integer colThree, col1, col2, col4, col5, col6, col7, col8, col9, col10, col11, col12,
 	col13, col14, col15, col16, col17, col18, col19, col20, col21, col22, col23, col24, col25;
 	
 	
@@ -263,6 +263,13 @@ public class User {
 	public void setCol25(Integer col25) {
 		this.col25 = col25;
 	}
+	 public int getId() {
+	        return id.get();
+	    }
+
+	    public void setId(int id) {
+	        this.id.set(id);
+	    }
 	
 	
 }
